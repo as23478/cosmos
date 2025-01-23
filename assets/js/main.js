f7fd1f30edbcc309225a13d8f73f5a091ac1a989
as23478 <tabby1018@naver.com>
2025-01-23 15:23:20 +0900

    v2.6.1
    테마 기능

--- assets/js/main.js
+++ assets/js/main.js
@@ -339,30 +339,30 @@
 	const faqItems = document.querySelectorAll('.faq-item');
 
 	faqItems.forEach(item => {
-	const question = item.querySelector('.faq-question');
-	const answer = item.nextElementSibling;
-	const icon = item.querySelector('i');
-	
-	item.addEventListener('click', () => {
-		faqItems.forEach(otherItem => {
-		if (otherItem !== item) {
-			const otherAnswer = otherItem.nextElementSibling;
-			const otherIcon = otherItem.querySelector('i');
-	
-			otherAnswer.classList.remove('active');
-			otherIcon.classList.remove('active');
-			otherAnswer.style.maxHeight = "0";
-		}
+		const question = item.querySelector('.faq-question');
+		const answer = item.nextElementSibling;
+		const icon = item.querySelector('i');
+		
+		item.addEventListener('click', () => {
+			faqItems.forEach(otherItem => {
+			if (otherItem !== item) {
+				const otherAnswer = otherItem.nextElementSibling;
+				const otherIcon = otherItem.querySelector('i');
+		
+				otherAnswer.classList.remove('active');
+				otherIcon.classList.remove('active');
+				otherAnswer.style.maxHeight = "0";
+			}
+			});
+		
+			answer.classList.toggle('active');
+			icon.classList.toggle('active');
+			if (answer.classList.contains('active')) {
+			answer.style.maxHeight = answer.scrollHeight + "px";
+			} else {
+			answer.style.maxHeight = "0";
+			}
 		});
-	
-		answer.classList.toggle('active');
-		icon.classList.toggle('active');
-		if (answer.classList.contains('active')) {
-		answer.style.maxHeight = answer.scrollHeight + "px";
-		} else {
-		answer.style.maxHeight = "0";
-		}
-	});
 	});
 
 /*
@@ -505,7 +505,6 @@
 	// 	};
 	// 	filterList.init();
 	// });
-
 
 
 
@@ -820,6 +819,12 @@
 
 	// 화면 크기 변경 시 다시 초기화
 	window.addEventListener('resize', initializeTabs);
+
+
+
+
+	
+
 	
 
 }(jQuery));
